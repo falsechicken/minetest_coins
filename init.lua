afede77d7ee27b73ed7aaa7fc8a19602d4588281
False_Chicken <jmdevsupport@gmail.com>
2015-02-04 15:28:38 -0500

    Changed references to coin textures to vars.

--- init.lua
+++ init.lua
@@ -1,16 +1,22 @@
 
+-- Coin Texture Vars
+local goldCoinTexture = "Gold_coin_icon_32.png"
+local silverCoinTexture = "Silver_coin_icon_16.png"
+local copperCoinTexture = "Copper_coin_icon_16.png"
+--
+
 -- Add Items
 minetest.register_craftitem("jcoins:goldcoin", {
 	description = "Gold Coin",
-	inventory_image = "Gold_coin_icon_16.png",
+	inventory_image = goldCoinTexture,
 	})
 	
 minetest.register_craftitem("jcoins:silvercoin", {
 	description = "Silver Coin",
-	inventory_image = "Silver_coin_icon_16.png",
+	inventory_image = silverCoinTexture,
 	})
 
 minetest.register_craftitem("jcoins:coppercoin", {
 	description = "Copper Coin",
-	inventory_image = "Copper_coin_icon_16.png",
+	inventory_image = copperCoinTexture,
 	})
