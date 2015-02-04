a81853340987adb85ede5075848012ecc60bff1c
False_Chicken <jmdevsupport@gmail.com>
2015-02-04 15:11:59 -0500

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+
+-- Add Items
+minetest.register_craftitem("jcoins:goldcoin", {
+	description = "Gold Coin",
+	inventory_image = "Gold_coin_icon_16.png",
+	})
+	
+minetest.register_craftitem("jcoins:silvercoin", {
+	description = "Silver Coin",
+	inventory_image = "Silver_coin_icon_16.png",
+	})
+
+minetest.register_craftitem("jcoins:coppercoin", {
+	description = "Copper Coin",
+	inventory_image = "Copper_coin_icon_16.png",
+	})
