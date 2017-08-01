9a4f638b73b2d66b933d9d845697623c8408b682
False_Chicken <jmdevsupport@gmail.com>
2017-08-01 16:51:59 -0400

    Changed max stack to 100.

--- init.lua
+++ init.lua
@@ -9,21 +9,28 @@ local copperCoinTexture = "Copper_coin_icon_32.png"
 wieldScale = { x = 0.5, y = 0.5, z = 0.5 }
 --
 
+-- Coin Max Stack Var
+stackMax = 100
+--
+
 -- Add Items
 minetest.register_craftitem("minetest_coins:goldcoin", {
 	description = "Gold Coin",
 	inventory_image = goldCoinTexture,
 	wield_scale = wieldScale,
+	stack_max = stackMax,
 	})
 	
 minetest.register_craftitem("minetest_coins:silvercoin", {
 	description = "Silver Coin",
 	inventory_image = silverCoinTexture,
 	wield_scale = wieldScale,
+	stack_max = stackMax,
 	})
 
 minetest.register_craftitem("minetest_coins:coppercoin", {
 	description = "Copper Coin",
 	inventory_image = copperCoinTexture,
 	wield_scale = wieldScale,
+	stack_max = stackMax,
 	})
