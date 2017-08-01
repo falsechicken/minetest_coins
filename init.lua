c6251c579bfe9f16f437f6be1b2fcdf62942bb8c
False_Chicken <jmdevsupport@gmail.com>
2017-08-01 01:43:09 -0400

    Changed coin inventory item texture size and wield scale.

--- init.lua
+++ init.lua
@@ -1,22 +1,29 @@
 
 -- Coin Texture Vars
 local goldCoinTexture = "Gold_coin_icon_32.png"
-local silverCoinTexture = "Silver_coin_icon_16.png"
-local copperCoinTexture = "Copper_coin_icon_16.png"
+local silverCoinTexture = "Silver_coin_icon_32.png"
+local copperCoinTexture = "Copper_coin_icon_32.png"
+--
+
+-- Coin Wield Scale Var
+wieldScale = { x = 0.5, y = 0.5, z = 0.5 }
 --
 
 -- Add Items
 minetest.register_craftitem("jcoins:goldcoin", {
 	description = "Gold Coin",
 	inventory_image = goldCoinTexture,
+	wield_scale = wieldScale,
 	})
 	
 minetest.register_craftitem("jcoins:silvercoin", {
 	description = "Silver Coin",
 	inventory_image = silverCoinTexture,
+	wield_scale = wieldScale,
 	})
 
 minetest.register_craftitem("jcoins:coppercoin", {
 	description = "Copper Coin",
 	inventory_image = copperCoinTexture,
+	wield_scale = wieldScale,
 	})
